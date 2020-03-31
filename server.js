ae826221d5684b34a54d034fc5d0f9197b2d98fa
Sam Delay <samuel.delay@gmail.com>
2020-03-31 15:39:41 -0400

    server commit test

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const db = require("./models");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars")
+
+// const routes = require("");
+
+// app.use(routes);
+
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
