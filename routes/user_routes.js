60714d7725c9a47b7d16c680c490b33c683a1536
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-04-04 11:16:03 -0400

    user routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,42 +3,53 @@ const router = express.Router();
 
 let db = require("../models");
 
-module.exports = function () {
-  // Route for getting all personal posts
-  router.get("/api/posts/:user", function (req, res) {
-    let query = {};
-    if (req.query.user_id) {
-      query.UserId = req.query.user_id;
-    }
-    db.Post.findAll({
-      where: query,
-      include: [db.User],
-    }).then(function (post) {
-      res.render("user", {
-        post,
-      });
+router.get("/api/posts/:user", function (req, res) {
+  let query = {};
+  if (req.query.user_id) {
+    query.UserId = req.query.user_id;
+  }
+  db.Post.findAll({
+    where: query,
+    include: [db.User],
+  }).then(function (post) {
+    res.render("user", {
+      post,
     });
   });
+});
 
-  // Route for employees to send feedback to specified admin
-  router.post("/api/newMessage", function (req, res) {
-    db.User.create(req.body).then(function (post) {
-      res.render("user", {
-        post,
-      });
+router.post("/api/newMessage", function (req, res) {
+  db.User.create(req.body).then(function (post) {
+    res.json(post);
+  });
+});
+
+router.get("/api/answers/:user", function (req, res) {
+  db.Drift_DB.findAll({
+    where: {
+      id: req.params.id,
+    },
+  }).then(function (post) {
+    res.render("user", {
+      post,
     });
   });
+});
 
-  router.post("/api/posts/:user", function (req, res) {
-    db.Drift_DB.findAll({
-      where: {
-        id: req.params.id,
-      },
-    }).then(function (post) {
-      res.render("user", {
-        post,
-      });
+router.post("/api/answers/:user", function (req, res) {
+  db.Answer.create({
+    answer: req.body.answer,
+  }).then(function (data) {
+    res.json(data);
+  });
+});
+
+router.get("/api/getQuestions", function (req, res) {
+  db.Question.findAll({}).then(function (question) {
+    res.render("user", {
+      question,
     });
-
+  });
+});
 
 module.exports = router;
