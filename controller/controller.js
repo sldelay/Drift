3136eae6dd32a70f92fa53d8c27eb9ac7239a3e2
Sam Delay <samuel.delay@gmail.com>
2020-04-05 16:17:14 -0400

    controller

---
+++ controller/controller.js
@@ -0,0 +1,37 @@
+let db = require("../models");
+
+module.exports = {
+  createQuestion: (data) => {
+    return db.Question.create({
+      category: data.category,
+      question: data.question,
+    });
+  },
+  createUser: (data) => {
+    return db.User.create({
+      name: data.name,
+      email: data.email,
+      admin: data.admin,
+    });
+  },
+  createCompany: (data) => {
+    return db.Company.create({
+      name: data.name,
+    });
+  },
+  createPost: (data) => {
+    return db.Post.create({
+      subject: data.subject,
+      category: data.category,
+      content: data.content,
+      UserId: data.userId,
+    });
+  },
+  createAnswer: (data) => {
+    return db.Answer.create({
+      value: data.value,
+      QuestionId: data.questId,
+      UserId: data.userId,
+    });
+  },
+};
