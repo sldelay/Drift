3445eb751602e845f35b22dab652726c19cd96df
Sam Delay <samuel.delay@gmail.com>
2020-04-05 16:17:31 -0400

    datafixture

--- datafixtures/datafixture.js
+++ datafixtures/datafixture.js
@@ -1,16 +1,93 @@
-const questControl = require("../controller/questioncontroller.js");
+const questControl = require("../controller/controller.js");
 
 let questArr = [
   { category: "asdfasdgasgd", question: "sdagasdgsad" },
   { category: "asdgfasdg", question: "asfasdgasdg" },
 ];
 
-let seedQuestions = function () {
-  for (const ele of questArr) {
-    questControl.createQuestion(ele);
-  }
+let postArr = [
+  {
+    subject: "Hey there",
+    category: "managment",
+    content: "Management sucks",
+    userId: 4,
+  },
+  {
+    subject: "Hey there",
+    category: "hours",
+    content: "Hours sucks",
+    userId: 3,
+  },
+  {
+    subject: "Hey there",
+    category: "coworker",
+    content: "Coworker is great",
+    userId: 2,
+  },
+  {
+    subject: "Hey there",
+    category: "coworker",
+    content: "Coworker sucks",
+    userId: 3,
+  },
+  {
+    subject: "Hey there",
+    category: "managment",
+    content: "Management is great",
+    userId: 4,
+  },
+  {
+    subject: "Hey there",
+    category: "hours",
+    content: "Hours are great",
+    userId: 2,
+  },
+];
+
+let userArr = [
+  { name: "Bob Williams", email: "bob@gmail.com", admin: true },
+  { name: "Sam Delay", email: "sam@gmail.com", admin: false },
+  { name: "Alyssa Williams", email: "alyssa@gmail.com", admin: false },
+  { name: "Mike", email: "mike@gmail.com", admin: false },
+  { name: "Bobby", email: "bobby@gmail.com", admin: true },
+];
+
+const seedQuestions = function () {
+  return new Promise((res, rej) => {
+    let promArr = [];
+    for (const ele of questArr) {
+      promArr.push(questControl.createQuestion(ele));
+    }
+    Promise.all(promArr).then(res).catch(rej);
+  });
+};
+
+const seedPosts = function () {
+  return new Promise((res, rej) => {
+    let promArr = [];
+    for (const ele of postArr) {
+      promArr.push(questControl.createPost(ele));
+    }
+    Promise.all(promArr).then(res).catch(rej);
+  });
+};
+
+const seedUser = function () {
+  return new Promise((res, rej) => {
+    let promArr = [];
+    for (const ele of userArr) {
+      promArr.push(questControl.createUser(ele));
+    }
+    Promise.all(promArr).then(res).catch(rej);
+  });
 };
 
 module.exports = function () {
-  seedQuestions();
+  seedQuestions()
+    .then(() => {
+      return seedUser();
+    })
+    .then(() => {
+      return seedPosts();
+    });
 };
