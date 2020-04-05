bcfb4804496b01de8ba11e7246509d376b55d72e
Walter Hoerman <whoerman@gmail.com>
2020-04-05 15:53:06 -0400

    forming an array from the landing page

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -1,8 +1,19 @@
 // JQuery for home/landing page goes here
-// ajax for getting form Data form form to /api/newcomapany in route-company_routes.js
 
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-// $(function () {
-//   $(".").on("click", function (event) {
-//   });
-// });
+$(document).ready(function () {
+  function arrayToObject(arr) {
+    const Obj = {};
+    arr.forEach((element) => {
+      Obj[element.name] = element.value;
+    });
+    return Obj;
+  }
+
+  //EVENTS
+  $("#newCompany").on("submit", function (event) {
+    event.preventDefault();
+    const formArray = $(event.currentTarget).serializeArray();
+    const formData = arrayToObject(formArray);
+    console.log(formData);
+  });
+});
