79c0ff8ab2f032fbbce2cd8fd4ab7b2ea1131ed6
Walter Hoerman <whoerman@gmail.com>
2020-04-05 15:00:12 -0400

    eslint changes

--- public/assets/js/landing.js
+++ public/assets/js/landing.js
@@ -1,1 +1,10 @@
 // JQuery for home/landing page goes here
+// ajax for getting form Data form form to /api/newcomapany in route-company_routes.js
+
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".").on("click", function (event) {
+        
+    });
+})
