b35769c657d479c83ee7cd0ddf70c259b51ac906
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-04-06 20:27:08 -0400

    routing for about,contact,instructions pages

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const adminRouter = require("./routes/admin_routes");
 const employeeRouter = require("./routes/user_routes");
+const htmlRouter = require("./routes/html_routes");
 const exphbs = require("express-handlebars");
 
 const app = express();
@@ -113,6 +114,7 @@ app.use("/", indexRouter);
 app.use("/", usersRouter);
 app.use("/", adminRouter);
 app.use("/", employeeRouter);
+app.use("/", htmlRouter);
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
