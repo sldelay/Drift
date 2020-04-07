c28e36c6d6fef41367a47f9c1198036746ef3159
Walter Hoerman <whoerman@gmail.com>
2020-04-06 22:06:15 -0400

    adding a contact form

---
+++ public/assets/js/contactus.js
@@ -0,0 +1,19 @@
+// JQuery for home/landing page goes here
+
+$(document).ready(function () {
+  function arrayToObject(arr) {
+    const Obj = {};
+    arr.forEach((element) => {
+      Obj[element.name] = element.value;
+    });
+    return Obj;
+  }
+
+  //EVENTS
+  $("#newContact").on("submit", function (event) {
+    event.preventDefault();
+    const formArray = $(event.currentTarget).serializeArray();
+    const contactData = arrayToObject(formArray);
+    console.log(contactData);
+  });
+});
