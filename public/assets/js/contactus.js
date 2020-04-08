672b332755515630620477e8d476ad829b80d747
Sam Delay <samuel.delay@gmail.com>
2020-04-08 16:04:28 -0400

    wrap in doc.ready

--- public/assets/js/contactus.js
+++ public/assets/js/contactus.js
@@ -1,19 +1,21 @@
 // JQuery for home/landing page goes here
 
 $(document).ready(function () {
-  function arrayToObject(arr) {
-    const Obj = {};
-    arr.forEach((element) => {
-      Obj[element.name] = element.value;
-    });
-    return Obj;
-  }
+  $(document).ready(function () {
+    function arrayToObject(arr) {
+      const Obj = {};
+      arr.forEach((element) => {
+        Obj[element.name] = element.value;
+      });
+      return Obj;
+    }
 
-  //EVENTS
-  $("#newContact").on("submit", function (event) {
-    event.preventDefault();
-    const formArray = $(event.currentTarget).serializeArray();
-    const contactData = arrayToObject(formArray);
-    console.log(contactData);
+    //EVENTS
+    $("#newContact").on("submit", function (event) {
+      event.preventDefault();
+      const formArray = $(event.currentTarget).serializeArray();
+      const contactData = arrayToObject(formArray);
+      console.log(contactData);
+    });
   });
 });
