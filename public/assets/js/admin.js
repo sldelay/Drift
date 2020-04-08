95cbd2a8c83aab8d3ff17a7674990426279d6f87
Sam Delay <samuel.delay@gmail.com>
2020-04-08 16:04:13 -0400

    editing event listener

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,32 +1,36 @@
 // JQuery for admin page goes here
-$(document).on("submit", ".newEmployee", function (event) {
-  event.preventDefault();
-  let postData = {
-    name: $("#exampleInputEmail1").val().trim(),
-    email: $("#exampleInputPassword1").val().trim(),
-    owner: false,
-    admin: $("#exampleFormControlSelect1").val().trim(),
-  };
-  $.ajax({
-    method: "POST",
-    url: "/api/newUser",
-    data: postData,
-  }).then(function () {
-    window.location.href = "/admin";
+$(document).ready(function () {
+  $(document).on("submit", "#newEmployee", function (event) {
+    event.preventDefault();
+    let postData = {
+      name: $("#exampleInputEmail1").val().trim(),
+      email: $("#exampleInputPassword1").val().trim(),
+      admin: $("#exampleFormControlSelect1").val(),
+    };
+    $.ajax({
+      method: "POST",
+      url: "/api/newUser",
+      data: postData,
+    }).then(function () {
+      location.reload();
+    });
   });
-});
 
-$(document).on("submit", ".newQuestion", function (event) {
-  event.preventDefault();
-  let postData = {
-    question: $("formQuestion").val().trim(),
-  };
-  $.ajax({
-    method: "POST",
-    url: "/api/newQuestion",
-    data: postData,
-  }).then(function () {
-    window.location.href = "/admin";
+  $(document).on("submit", "#newQuestion", function (event) {
+    event.preventDefault();
+    let postData = {
+      question: $("#formQuestion").val(),
+      category: $("#formCategory").val(),
+    };
+    console.log(postData);
+    $.ajax({
+      method: "POST",
+      url: "/api/newQuestion",
+      data: postData,
+    }).then(function (data) {
+      console.log(data);
+      location.reload();
+    });
   });
 });
 
