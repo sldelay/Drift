1b9c90e1eb82e69173c1bc4a4de862ce6e61f46b
Sam Delay <samuel.delay@gmail.com>
2020-04-09 16:49:40 -0400

    heroku second try

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. SD removed "next" argument */
 router.get("/", function (req, res) {
-  res.render("index", {});
+  res.render("index");
 });
 
 module.exports = router;
