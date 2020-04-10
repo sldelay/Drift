cb062b65afb4bcdb61904f8678993cd9d1f723a6
Sam Delay <samuel.delay@gmail.com>
2020-04-09 21:38:17 -0400

    heroku

--- config/config.js
+++ config/config.js
@@ -9,20 +9,8 @@ module.exports = {
     port: 3306,
     dialect: "mysql",
   },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: 3306,
-    dialect: "mysql",
-  },
   production: {
-    username: process.env.HR_USER,
-    password: process.env.HR_PASSWORD,
-    database: process.env.HR_NAME,
-    host: process.env.HR_HOST,
-    port: 3306,
+    use_env_variable: JAWS,
     dialect: "mysql",
   },
 };
